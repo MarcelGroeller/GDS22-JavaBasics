5a0f549e1311a16597c64fcd27267be6d7e952fd
MarcelGroeller <01618478@edu.campus02.at>
2022-09-23 17:10:22 +0200

    My First Class created; args as input parameter written to console!

---
+++ MyFirstClass.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    //wir haben unsere erste Klasse erzeugt
+
+    //psvm
+    public static void main(String[] args) {
+
+        //erste Ausgabe (sout+TAB)
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        //args
+        //kürzel sout+TAB
+        System.out.println(Arrays.toString(args));
+    }
+}
